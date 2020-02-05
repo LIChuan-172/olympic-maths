1af9b17fbedc156e8555594065b23f279b9dfb72
LI Chuan <lichuan172@gmail.com>
2020-02-05 19:51:53 +0800

    change data

--- src/Components/Multy/getMultyQueues.js
+++ src/Components/Multy/getMultyQueues.js
@@ -10,27 +10,28 @@ function getMultyQueues() {
     ],
     [
       { answer: 1, value: 1 },
-      { answer: 3, value: 3 },
-      { answer: 5, value: 5 },
-      { answer: 7, value: "" },
-      { answer: 9, value: "" },
-      { answer: 11, value: 11 }
+      { answer: 9, value: 9 },
+      { answer: 25, value: 25 },
+      { answer: 49, value: "" },
+      { answer: 81, value: "" },
+      { answer: 121, value: 121 }
     ],
     [
-      { answer: 1, value: 1 },
-      { answer: 3, value: 3 },
-      { answer: 5, value: 5 },
-      { answer: 7, value: "" },
-      { answer: 9, value: "" },
-      { answer: 11, value: 11 }
+
+      { answer: 4, value: 4 },
+      { answer: 8, value: 8 },
+      { answer: 16, value: 16 },
+      { answer: 32, value: "" },
+      { answer: 64, value: "" },
+      { answer: 128, value: 128}
     ],
     [
-      { answer: 1, value: 1 },
       { answer: 3, value: 3 },
-      { answer: 5, value: 5 },
-      { answer: 7, value: "" },
-      { answer: 9, value: "" },
-      { answer: 11, value: 11 }
+      { answer: 6, value: 6 },
+      { answer: 9, value: 9 },
+      { answer: 12, value: "" },
+      { answer: 15, value: "" },
+      { answer: 18, value: 18 }
     ]
   ];
 }
